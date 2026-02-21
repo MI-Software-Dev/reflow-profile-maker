9c3ac33dbd42c646d4c2caa7cea3f2951f532cb5
MI-Software-Dev <misoftdev@outlook.com>
2026-02-21 22:03:42 +0700

    Add redirect to kit-reflow-number page on home load

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/kit-reflow-number");
+  }, [router]);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
   );
