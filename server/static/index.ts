4cac41e2d0f7287e94ed05c9fa0f263201f72bad
MI-Software-Dev <misoftdev@outlook.com>
2026-02-22 21:37:01 +0700

    stop tracking server/static

--- server/static/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as groupConfigJson } from "./group-config.json";
-export { default as lineConfigJson } from "./line-config.json";
-export { default as n2ConfigJson } from "./n2-config.json";
-export { default as nasConfigJson } from "./nas-config.json";
