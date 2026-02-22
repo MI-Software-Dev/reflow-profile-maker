14667466b30be3cf6e82f43ef175887af90c3cec
MI-Software-Dev <misoftdev@outlook.com>
2026-02-22 21:31:47 +0700

    Add type annotation for nasConfig variable

--- server/routes/v1/setting/use-cases/upload-result.ts
+++ server/routes/v1/setting/use-cases/upload-result.ts
@@ -1,8 +1,9 @@
 import { Readable } from "stream";
 import { withFTP } from "../../../../wrapper";
 import { staticClient } from "../../../../utils";
+import { nasConfigJson } from "@/server/static";
 export const UploadResultUseCase = async (file: File, isNg: boolean) => {
-  const nasConfig = staticClient.readStatic("nas");
+  const nasConfig: typeof nasConfigJson = staticClient.readStatic("nas");
   const arrayBuffer = await file.arrayBuffer();
   const stream = Readable.from(Buffer.from(arrayBuffer));
   const fileName = file.name;
