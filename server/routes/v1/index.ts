29d3c2109c589ea48795ea1550af4663de239bb4
MI-Software-Dev <misoftdev@outlook.com>
2026-02-22 21:31:39 +0700

    Add KitReflowRoute to V1 API routes

--- server/routes/v1/index.ts
+++ server/routes/v1/index.ts
@@ -1,4 +1,7 @@
 import Elysia from "elysia";
 import { SettingRoute } from "./setting";
+import { KitReflowRoute } from "./kit-reflow";
 
-export const V1Route = new Elysia({ prefix: "/v1" }).use(SettingRoute);
+export const V1Route = new Elysia({ prefix: "/v1" })
+  .use(SettingRoute)
+  .use(KitReflowRoute);
