6b6f33b15a91811fb9ad77c6713c9d5fbfa316e9
MI-Software-Dev <misoftdev@outlook.com>
2026-02-24 10:16:27 +0700

    Add form field components to molecules index exports

--- client/components/molecules/index.ts
+++ client/components/molecules/index.ts
@@ -1,3 +1,6 @@
+export { SearchField } from "./SearchField";
 export { Table } from "./Table";
 export { Navbar } from "./Navbar";
-
+export { SelectFormField } from "./SelectFormField";
+export { FormField } from "./FormField";
+export { FileInputField } from "./FileInputField";
