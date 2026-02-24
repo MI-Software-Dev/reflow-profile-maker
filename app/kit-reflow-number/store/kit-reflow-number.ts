fca543b6374927aa7214b69ab561aa626e107cb8
MI-Software-Dev <misoftdev@outlook.com>
2026-02-24 10:12:42 +0700

    Add kit reflow data management to store
    Implement init, search, and fetch methods with error handling using
    SweetAlert2 for user notifications

--- app/kit-reflow-number/store/kit-reflow-number.ts
+++ app/kit-reflow-number/store/kit-reflow-number.ts
@@ -1,9 +1,42 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
+import { KitReflow } from "../schema";
+import { FetchKitReflowUseCase } from "../use-cases";
+import Swal from "sweetalert2";
 
 class KitReflowNumberStore {
+  kitReflows: KitReflow[] = [];
   constructor() {
     makeAutoObservable(this);
   }
+
+  init = async () => {
+    await this.fetchKitReflow();
+  };
+
+  onSearch = async (val: string) => {
+    if (val.length == 0) {
+      await this.fetchKitReflow();
+    } else {
+      this.kitReflows = this.kitReflows.filter((item) =>
+        item.kitName.includes(val),
+      );
+    }
+  };
+
+  fetchKitReflow = async () => {
+    const res = await FetchKitReflowUseCase();
+    if (res.success) {
+      runInAction(() => {
+        this.kitReflows = res.data.items;
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Loading Kit Reflow Error",
+        text: res.error,
+      });
+    }
+  };
 }
 
 export const kitReflowNumberStore = new KitReflowNumberStore();
