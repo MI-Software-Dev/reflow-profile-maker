7bd288c99c77426f33c69d8441b6c3eae639552c
MI-Software-Dev <misoftdev@outlook.com>
2026-02-24 10:11:16 +0700

    Add kit reflow number page with search and table
    Implement complete page functionality including store integration,
    search field for kit names, and table display with kit name and
    reflow number columns.

--- app/kit-reflow-number/page.tsx
+++ app/kit-reflow-number/page.tsx
@@ -1,12 +1,44 @@
 "use client";
-import { PageContent } from "@/client/components/atoms";
 import { observer } from "mobx-react";
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { kitReflowNumberStore } from "./store";
+import { SearchField, Table } from "@/client/components/molecules";
+import { TableColumn } from "@/client/components/molecules/Table/type";
+import { KitReflow } from "./schema";
+import { PageContent } from "@/client/components/templates";
 
 const Page: FC = observer(() => {
+  const { kitReflows, onSearch } = kitReflowNumberStore;
+  const columns: TableColumn<KitReflow>[] = [
+    {
+      header: "Kit name",
+      key: "kitName",
+      sortable: true,
+      render: (val) => {
+        return val;
+      },
+    },
+    {
+      header: "Reflow number",
+      key: "reflowNumber",
+      render: (val) => {
+        return val;
+      },
+    },
+  ];
+  useEffect(() => {
+    kitReflowNumberStore.init();
+  }, []);
   return (
     <PageContent>
-      <></>
+      <div className="flex flex-col gap-2 p-4">
+        <SearchField
+          size="md"
+          placeholder="Seach kit names..."
+          onSearch={onSearch}
+        />
+        <Table data={kitReflows} columns={columns} />
+      </div>
     </PageContent>
   );
 });
