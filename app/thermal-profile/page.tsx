32a3dd83add1169850a6da72c74f221bebebe8d0
MI-Software-Dev <misoftdev@outlook.com>
2026-02-24 10:19:14 +0700

    Add thermal profile page with empty content

---
+++ app/thermal-profile/page.tsx
@@ -0,0 +1,11 @@
+import { PageContent } from "@/client/components/templates";
+import { FC } from "react";
+
+const Page: FC = () => {
+  return (
+    <PageContent>
+      <></>
+    </PageContent>
+  );
+};
+export default Page;
