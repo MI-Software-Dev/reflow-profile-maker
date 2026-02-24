6538accabe4a8fa4119e37701897ea7ea5d81a7b
MI-Software-Dev <misoftdev@outlook.com>
2026-02-24 10:13:38 +0700

    Remove all exports from atoms index file

--- client/components/atoms/index.ts
+++
@@ -1,4 +0,0 @@
-export { SelectFormField } from "./SelectFormField";
-export { FormField } from "./FormField";
-export { PageContent } from "./PageContent";
-export { FileInputField } from "./FileInputField";
