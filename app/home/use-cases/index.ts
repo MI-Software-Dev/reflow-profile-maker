509830bc25b8e99aa8f618a6b398aff3204a70fb
MI-Software-Dev <misoftdev@outlook.com>
2026-02-24 10:10:56 +0700

    Add FetchStaticFormUseCase export to home use cases

--- app/home/use-cases/index.ts
+++ app/home/use-cases/index.ts
@@ -1,2 +1,3 @@
+export { FetchStaticFormUseCase } from "./fetch-static-form";
 export { ReadMemoryTempTmrUseCase } from "./read-memory-temp-tmr";
 export { ReadMemoryTempCsvUseCase } from "./read-memory-temp-csv";
