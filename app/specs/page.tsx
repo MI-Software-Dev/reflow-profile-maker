20fa0eb9adcb71b115c571d467fe533509941f07
MI-Software-Dev <misoftdev@outlook.com>
2026-02-24 10:12:59 +0700

    Remove unused form components and specs page

--- app/specs/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PageContent } from "@/client/components/atoms";
-import { FC } from "react";
-
-const Page: FC = () => {
-  return <PageContent>
-    <></>
-  </PageContent>;
-};
-export default Page;
